3cf4a78ef9c49a3a4381d7ae0a7b9b4f09f9b3b5
Trysuci1811 <113281891+Trysuci1811@users.noreply.github.com>
2024-07-15 13:17:57 +0700

    Update ketentuan.blade.php

--- resources/views/pasien/pendaftaran/ketentuan.blade.php
+++ resources/views/pasien/pendaftaran/ketentuan.blade.php
@@ -8,27 +8,35 @@
                 <div>
                     <ol class="max-w-full space-y-1 text-gray-500 list-decimal list-inside dark:text-gray-400">
                         <li class="text-lg">
-                            Pendaftaran online hanya bisa dilakukan oleh pasien yang telah memiliki nomor rekam medis
+                            Pendaftaran online dibuka dari pukul <b>07. 00 – 21.00</b> WIB setiap hari
                         </li>
                         <hr>
                         <li class="text-lg">
-                            Pelayanan pendaftaran online dilakukan pukul <b>07. 00 – 21.00</b> WIB
+                            Pendaftaran online dapat dilakukan mulai <b>H-5 sampai H-2</b> kunjungan 
                         </li>
                         <hr>
                         <li class="text-lg">
-                            Pendaftaran online di lakukan <b>satu hari (H-6)</b> sebelum periksa
+                            Khusus pasien BPJS harus menyiapkan SKDP/Surat Rujukan untuk diupload ke dalam sistem pendaftaran online dalam bentuk png/jpg dan diharapkan dibawa juga ke rumah sakit saat verifikasi kedatangan
                         </li>
                         <hr>
                         <li class="text-lg">
-                            Pilihan klinik yang dituju tidak bisa dipilih jika kuota sudah penuh
+                            Pilihan dokter tidak bisa dipilih jika kuota sudah penuh dan jam praktek tidak tersedia
                         </li>
                         <hr>
                         <li class="text-lg">
-                            Khusus pasien BPJS harus menyiapkan SKDP/Surat Rujukan untuk diupload ke dalam sistem pendaftaran online dalam bentuk pdf/png/jpg dan diharapkan dibawa juga ke rumah sakit saat verifikasi kedatangan
+                            Kuota klinik spesialis bagi pasien BPJS sebanyak 5 pasien, sedangkan pasien umum 25 pasien
                         </li>
                         <hr>
                         <li class="text-lg">
-                            Setelah melakukan pendaftaran, anda akan mendapatkan bukti pendaftaran dan diharapkan untuk mengunduh atau ss bukti pendaftaran tersebut
+                            Kuota klinik gigi umum, klinik umum, dan klinik penunjang bagi pasien BPJS dan umum masing-masing sebanyak 25 pasien
+                        </li>
+                        <hr>
+                        <li class="text-lg">
+                            Khusus klinik integrasi (pasien mahasiswa) tidak terdapat batasan kuota
+                        </li>
+                        <hr>
+                        <li class="text-lg">
+                            Setelah melakukan pendaftaran, anda akan mendapatkan bukti pendaftaran dan diharapkan untuk mengunduh atau screenshot bukti pendaftaran tersebut 
                         </li>
                         <hr>
                         <li class="text-lg">
@@ -40,8 +48,8 @@
                         </li>
                         <hr>
                         <li class="text-lg">
-                            Diharapkan untuk memasukkan nomor handphone aktif dan terdaftar whatsapp untuk mengabari informasi, apabila sewaktu-waktu terdapat perubahan jadwal dokter
-                        </li>
+                            Diharapkan untuk memasukkan nomor handphone aktif dan terdaftar dengan Whatsapp untuk mengabari informasi, apabila sewaktu-waktu terdapat perubahan jadwal dokter
+                        </li>  
                         <hr>
                         <li class="text-lg">
                             Poliklinik libur di hari sabtu, minggu, dan tanggal merah
